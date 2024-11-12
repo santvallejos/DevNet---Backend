d043c2704f84b45f702b37e357027ff2fa6ba169
juanmumbach <juanimumbach@gmail.com>
2024-11-11 22:26:53 -0300

    Se agrego el DBContext al program.cs de la PL. Tambien se agrego dependencia de la PL a la DAL

--- DevNet-WebAPI/Program.cs
+++ DevNet-WebAPI/Program.cs
@@ -1,6 +1,18 @@
+using DevNet_DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Añadir Entity Framework Core al proyecto y configurar opciones de DbContext
+builder.Services.AddDbContext<DevnetDBContext>
+    (
+        options =>
+            options.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection")
+            )
+    );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
